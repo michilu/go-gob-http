36a8cf0a0675c3866cda33914612df6d83553814
ENDOH takanao <djmchl@gmail.com>
2015-01-16 13:50:06 +0700

    re-write copyrights

--- golang_11.go
+++ golang_11.go
@@ -1,4 +1,8 @@
+// Copyright 2015 ENDOH takanao.
+// <https://github.com/MiCHiLU/go-gob-http>
+//
 // Copyright 2014 Orchestrate, Inc.
+// <https://github.com/orchestrate-io/dvr>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
